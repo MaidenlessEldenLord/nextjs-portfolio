4dac1bda7aec5cefb129f0c1f5fe908d3a1c3aed
Jamal Gregg <102499266+MaidenlessEldenLord@users.noreply.github.com>
2024-08-12 17:48:38 -0400

    Create About.jsx

---
+++ src/app/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <main className="mt-40">
+        <div>
+            <h2 className="mb-20 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">About Me</h2>
+            <p className="mb-8 lg:mb-16 font-semibold text-center text-white dark:text-white sm:text-xl">Hello, my name is Jamal Gregg and that is my daughter, Leilani, in the photo(she's the real software developer typing away when I'm not looking). I'm a Front-End and Web Developer who graduated from Nucamp Coding Bootcamp with certifications in Front-End Web and Mobile Development, Back-End Development, and even Full Stack Development as well. In my spare time I try to pour in as much time I can coding any project or solving any coding challenges on CodeWars or Hackerrank. I'm currently job hunting at the present moment so I am open to remote roles as I like to be able to spend time with my daughter and have a work-life balance that works best for me. Please feel free to contact me with the form down below with serious inquiries.</p>
+        </div>
+    </main>
+  )
+}
+
+export default About
